5df95688ef1690ddebbae743b22262309ae37427
Carolina Medina <carolina.medina@usach.cl>
2021-01-28 05:22:37 -0300

    added time

---
+++ main.js
@@ -0,0 +1,24 @@
+function textoFecha(fecha) {
+
+    var numDiaSem = fecha.getDay();
+
+
+    var diasSemana = new Array("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado");
+    var diaLetras = diasSemana[fecha.getDay()];
+
+
+    var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+    var mesLetras = meses[fecha.getMonth()];
+
+    var diaMes = (fecha.getDate());
+    var anho = fecha.getFullYear();
+    var hora = fecha.getHours();
+    var min = fecha.getMinutes();
+
+    if ((min >= 0) && (min < 10)) { //añadde un cero cuando los minutostienen 1 dígito.
+        min = "0" + min;
+    }
+
+    var devolver = "Hoy es " + diaLetras + ", " + diaMes + " de " + mesLetras + " de " + anho + " y son las " + hora + ":" + min + " horas.";
+    return devolver;
+}
