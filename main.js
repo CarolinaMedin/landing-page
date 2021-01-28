e030ad2aaf54beb1af2e15eaf5bbc70937c3309c
Carolina Medina <carolina.medina@usach.cl>
2021-01-28 05:33:43 -0300

    added style time

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-function textoFecha(fecha) {
+function textTime(time) {
 
-    var numDiaSem = fecha.getDay();
-
-
-    var diasSemana = new Array("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado");
-    var diaLetras = diasSemana[fecha.getDay()];
+    let numDiaSem = time.getDay();
 
 
-    var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-    var mesLetras = meses[fecha.getMonth()];
+    let diasSemana = new Array("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado");
+    let diaLetras = diasSemana[time.getDay()];
 
-    var diaMes = (fecha.getDate());
-    var anho = fecha.getFullYear();
-    var hora = fecha.getHours();
-    var min = fecha.getMinutes();
+
+    let meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+    let mesLetras = meses[time.getMonth()];
+
+    let diaMes = (time.getDate());
+    let anho = time.getFullYear();
+    let hora = time.getHours();
+    let min = time.getMinutes();
 
     if ((min >= 0) && (min < 10)) { //añadde un cero cuando los minutostienen 1 dígito.
         min = "0" + min;
     }
 
-    var devolver = "Hoy es " + diaLetras + ", " + diaMes + " de " + mesLetras + " de " + anho + " y son las " + hora + ":" + min + " horas.";
+    let devolver = "Hoy es " + diaLetras + ", " + diaMes + " de " + mesLetras + " de " + anho + " y son las " + hora + ":" + min + " horas.";
     return devolver;
 }
+
+let time = new Date();
+date.innerHTML = textTime(time);
